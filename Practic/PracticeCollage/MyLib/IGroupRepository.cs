7839c76c455516d754c990b39307901e05a87b53
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-09-10 19:19:11 +0300

    Создал прокект про зачисления в колледж студентов. Добавил классы Student, Group. Добавил интерфейсы. Добавил класс CollageService.

---
+++ Practic/PracticeCollage/MyLib/IGroupRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public interface IGroupRepository
+    {
+        Group GetGroup(int id);
+        void UpdateGroup(Group group);
+    }
+}
---
+++ Practic/PracticeCollage/PracticeCollage/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PracticeCollage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PracticeCollage")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f0d07d1-a5f7-4f00-b683-8e2583e99b85")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
