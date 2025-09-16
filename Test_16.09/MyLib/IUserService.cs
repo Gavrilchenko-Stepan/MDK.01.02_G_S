c06fb98e6dded0ab036d1a9b19b4d59d67353e36
DmtrDv <st56@gmail.com>
2025-09-16 11:35:05 +0300

    Добавил интерфейс IUserService

---
+++ Test_16.09/MyLib/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public interface IUserService
+    {
+        User GetUser(int userId);
+        bool UserExists(int userId);
+    }
+}
